2e1723dc400c9184218de2a02b4d0cb7b187cf2f
JenJens348 <127061679+JenJens348@users.noreply.github.com>
2023-06-01 23:29:51 +1000

    Add files via upload
    Student Name: Jennifer Huang
    Student Number: S3482613

---
+++ A3 - Browser-based Interactive Project - 40%/script.js
@@ -0,0 +1,56 @@
+const parent = document.getElementsByClassName('div1');
+
+for (const box of parent) {
+  div1.addEventListener('click', (event) => {
+    event.target.remove();
+  });
+
+for (const box of parent) {
+  div2.addEventListener('click', (event) => {
+    event.target.remove();
+  });}
+
+for (const box of parent) {
+  div3.addEventListener('click', (event) => {
+    event.target.remove();
+  });}
+
+  for (const box of parent) {
+    div4.addEventListener('click', (event) => {
+      event.target.remove();
+    });}
+
+  for (const box of parent) {
+    div5.addEventListener('click', (event) => {
+      event.target.remove();
+    });}
+
+  for (const box of parent) {
+    div6.addEventListener('click', (event) => {
+      event.target.remove();
+    });}
+
+  for (const box of parent) {
+    div7.addEventListener('click', (event) => {
+      event.target.remove();
+    });}
+  for (const box of parent) {
+    div8.addEventListener('click', (event) => {
+      event.target.remove();
+    });}
+  for (const box of parent) {
+    div8.addEventListener('click', (event) => {
+      event.target.remove();
+    });}
+
+  for (const box of parent) {
+    div9.addEventListener('click', (event) => {
+      event.target.remove();
+    });}
+
+  for (const box of parent) {
+    div10.addEventListener('click', (event) => {
+      event.target.remove();
+    });}
+
+}
